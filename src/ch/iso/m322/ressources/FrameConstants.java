968d62fd0e4043a0a2a19d685ea69fa40f789b29
AlexMarrer <95776537+RayMatador@users.noreply.github.com>
2023-11-24 23:31:27 +0100

    d

--- src/ch/iso/m322/ressources/FrameConstants.java
+++
@@ -1,8 +0,0 @@
-package ch.iso.m322.ressources;
-
-public class FrameConstants {
-	
-	public static final String FRAME_TITLE = "PRÜFUNG - MyGUI";
-	
-	public static final String FRAME_BUTTON_ADD = "Add";
-}
